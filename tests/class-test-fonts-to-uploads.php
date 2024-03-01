6070c8f8df319b09f7b2574b5b14586a571e3d54
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-03-02 10:45:01 +1100

    Test that the font directories and URLs are correctly prefixed.

--- tests/class-test-fonts-to-uploads.php
+++ tests/class-test-fonts-to-uploads.php
@@ -15,9 +15,33 @@ use WP_UnitTestCase;
 class Test_Fonts_To_Uploads extends WP_UnitTestCase {
 
 	/**
-	 * Ensure tests are running.
+	 * Ensure font directory begins with uploads base path.
+	 *
+	 * @dataProvider data_font_directory_begins_with_upload_base_path
+	 *
+	 * @param string       $key         The key to compare.
+	 * @param string|false $uploads_key The uploads key to check against.
 	 */
-	public function test_sample() {
-		$this->assertTrue( true );
+	public function test_font_directory_begins_with_upload_base_path( $key, $uploads_key ) {
+		$uploads = wp_get_upload_dir();
+		$fonts   = wp_get_font_dir();
+
+		$this->assertArrayHasKey( $key, $uploads, "Uploads[{$uploads_key}] is not set." );
+		$this->assertArrayHasKey( $key, $fonts, "Fonts[{$key}] is not set." );
+		$this->assertStringStartsWith( $uploads[ $uploads_key ], $fonts[ $key ], "Fonts[{$key}] does not begin with uploads[{$uploads_key}]." );
+	}
+
+	/**
+	 * Data provider for test_font_directory_begins_with_upload_base_path
+	 *
+	 * @return array[] Data Provider
+	 */
+	public function data_font_directory_begins_with_upload_base_path() {
+		return array(
+			array( 'path', 'basedir' ),
+			array( 'url', 'baseurl' ),
+			array( 'basedir', 'basedir' ),
+			array( 'baseurl', 'baseurl' ),
+		);
 	}
 }
